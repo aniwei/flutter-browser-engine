d3d9064bc4bee1e1264a9cf00e25542c7943c861
weiyanhai <weiyanhai@jd.com>
2022-05-08 05:21:30 +0800

    update

--- __tests__/CanvasKit.test.ts
+++ __tests__/CanvasKit.test.ts
@@ -1,48 +1,31 @@
 import fs from 'fs'
-import path from 'path'
 import jimp from 'jimp'
+import { resolve } from 'path'
 import { Skia, SkiaFilterQuality } from '@Skia'
 import { CkPath, CkCanvas, CkPaint, CkBlurImageFilter, CkMatrixImageFilter } from '@CanvasKit'
 import { Canvas } from 'canvaskit-wasm'
 
 test(`Skia`, async () => {
-  await Skia.malloc(path.resolve(__dirname, 'canvaskit.wasm'))
+  await Skia.malloc(resolve(__dirname, 'canvaskit.wasm'))
 
   const surface = Skia.MakeSurface(100, 100)
 
-  const ckPath = CkPath.malloc()
+  const path = CkPath.malloc()
   const paint = CkPaint.malloc()
-  
-  const ckCanvas = CkCanvas.malloc(surface?.getCanvas() as Canvas)
+  const canvas = CkCanvas.malloc(surface?.getCanvas() as Canvas)
 
 
-  const imageFilter = CkBlurImageFilter.malloc({
-    sigmaX: 0,
-    sigmaY: 0,
-    tileMode: Skia.TileMode.Clamp
-  })
-  const matrixImageFilter = CkMatrixImageFilter.malloc({
-    matrix: Float32Array.from([
-      0, 4, 12, // Row 1
-      1, 5, 13, // Row 2
-      3, 7, 15, // Row 3
-    ]),
-    filterQuality: SkiaFilterQuality.Low
-  })
-
-  ckPath.moveTo(10, 10)
-  ckPath.lineTo(50, 50)
-  ckPath.lineTo(20, 10)
-  ckPath.lineTo(10, 20)
+  path.moveTo(10, 10)
+  path.lineTo(50, 50)
+  path.lineTo(20, 10)
+  path.lineTo(10, 20)
 
   paint.style = Skia.PaintStyle.Fill
 
-  ckCanvas.drawPath(ckPath, paint)
+  canvas.drawPath(path, paint)
 
   surface?.flush()
   const img = surface?.makeImageSnapshot();
-
   const base64 = 'data:image/png;base64,' + Buffer.from(img?.encodeToBytes() as ArrayBuffer).toString('base64')
-
-  debugger
+  
 })
